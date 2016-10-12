f24b5ecf65e9753be8a4f635db86e74cc9e315a1
Christopher Hannah <christopher.hannah@me.com>
2016-10-12 21:40:09 +0100

    Initial Upload
    The first “working” version of my upload manager.
    
    No admin page, or general upload view. Just a website to view the
    contents of a MySQL database pretty much.

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html>
+
+<?php
+    
+    $id = 1;
+    
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        $id = (int) $_GET['id'];
+    }
+    
+    $connection = new mysqli("127.0.0.1", "uploader", "upload123", "uploads");
+    
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+    
+    $sql = "SELECT `title`, `description`, `date`, `file`, `views`, `type` FROM uploads WHERE id = $id";
+    $result = $connection->query($sql);
+    $upload = $result->fetch_assoc();
+    
+    $title = $upload['title'];
+    $description =  $upload['description'];
+    $date =  $upload['date'];
+    $file =  $upload['file'];
+    $views =  $upload['views'];
+    $type =  $upload['type'];
+    
+    $pageURL = "http://www.chrishannah.me/uploads/file.php?id=$id";
+    $directURL = "http://www.chrishannah.me/uploads/$file";
+    
+    $views++;
+    $sql = "UPDATE `uploads` SET `views` = $views WHERE `id` = $id";
+    $result = $connection->query($sql);
+    
+    $mediaType = "something";
+    
+    switch ($type) {
+        case "mp4":
+        case "mkv":
+        case "mov":
+            $mediaType = "video";
+            break;
+        case "jpg":
+        case "jpeg":
+        case "gif":
+        case "png":
+        default: 
+            $mediaType = "image";
+            break;
+    }
+    
+?>
+
+
+<head>
+    <title>Chris Hannah | <?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    
+<body>
+    <div id="header">
+        <strong>Chris Hannah</strong> Uploads
+    </div>
+    <div id="content">
+        <h1><?php echo $title; ?></h1>
+        <p>
+            <?php echo $views ?> views
+        </p>
+        
+        <p>
+            <?php
+                if ($mediaType == "image") {
+                    echo '<img src="';
+                    echo $file;
+                    echo '">';
+                }
+                
+                if ($mediaType == "video") {
+                    echo '<video src="';
+                    echo $file;
+                    echo '" controls/>';
+                }
+            
+            ?>
+        </p>
+        
+        <p>
+            <strong>Date</strong> <br />
+            <?php echo $date; ?></p>
+        <p>
+            <strong>Description</strong> <br />
+            <?php echo $description; ?>
+        </p>
+        <div id="links">
+            <strong>Links</strong><br />
+            <ul>
+                <li><a href="<?php echo $pageURL; ?>">Web Page</a></li>
+                <li><a href="<?php echo $directURL; ?>">Direct Link</a></li>
+                
+                <?php 
+                    $tweet = "$title via @chrishannah";
+                ?>
+                    
+                <li><a href="https://twitter.com/intent/tweet" class="twitter-mention-button" data-size="large" data-text="<?php echo $tweet; ?>" data-related="chrishannah" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></li>
+            </ul>
+        </div>
+    </div>
+    <div id="footer">
+        <a href="http://www.twitter.com/chrishannah">Chris Hannah</a> &copy; 2016
+    </div>
+</body>
+    
+</html>
