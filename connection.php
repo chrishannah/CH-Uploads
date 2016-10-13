53284076b409fdf580bbf2160111c24763fd0a3b
Christopher Hannah <christopher.hannah@me.com>
2016-10-13 10:43:11 +0100

    Start work on dashboard

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $connection = new mysqli("127.0.0.1", "uploader", "upload123", "uploads");
+    $connection = new mysqli("localhost", "uploader", "uploader123", "uploads");
     
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
