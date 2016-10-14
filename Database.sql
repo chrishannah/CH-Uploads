55f573db2fa3d274b3f9d87364b57cf869adcef2
Christopher Hannah <christopher.hannah@me.com>
2016-10-14 15:55:55 +0100

    Create Database!
    The SQL code to create the upload table in MySQL for the site.

---
+++ Database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `uploads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(50),
+  `description` varchar(200),
+  `date` datetime,
+  `file` varchar(100),
+  `views` int(11),
+  `type` varchar(4)
+  );
