5ccea9a88b1d013606ab8dcac37a91e34d3407ae
Christopher Hannah <christopher.hannah@me.com>
2016-10-14 11:40:27 +0100

    Fix file type
    It now detects the file extension from the file name, because for some
    strange reason it wasn’t in the $_FILE data.

--- upload.php
+++ upload.php
@@ -16,22 +16,19 @@
          $title = mysqli_real_escape_string($connection, $_POST["title"]);
          $description = mysqli_real_escape_string($connection, $_POST["description"]);
          $file_name = mysqli_real_escape_string($connection, $file_name);
-         $type = mysqli_real_escape_string($connection, $_POST['uploadFile']['type']);
+         $type =  pathinfo($file_name, PATHINFO_EXTENSION);
          
          $sql = "INSERT INTO uploads (title, description, date, file, views, type) VALUES ('$title', '$description', NOW(), '$file_name', '0', '$type')";
-         echo '<p>';
-         echo $sql;
-         echo '</p>';
          
          $result = $connection->query($sql);
          
          $sql = "SELECT COUNT(id) FROM uploads;";
-         echo $sql;
+
          $result = $connection->query($sql);
          $upload = $result->fetch_assoc();
          $id = $upload["COUNT(id)"];
          
-         header('Location: http://chrishannah.me/uploads/file.php?id='.$id) ;
+         header('Location: file.php?id='.$id) ;
          
       }else{
          print_r($errors);
